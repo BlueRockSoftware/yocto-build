064e96f8ab03927f28eba8e450fb1e33afd3419c
Ben Payne <ben@bluerocksoft.com>
2023-05-20 14:54:08 -0700

    Testing for upload steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -l
 
 echo $GITHUB_WORKSPACE
-echo $(pwd)
-ls -la
+echo $1
 
 chown -R yocto:yocto $GITHUB_WORKSPACE
 
-ls -la /
 
 sudo -i -u yocto /build.sh $1 $GITHUB_WORKSPACE
+
+ls -la
+
+ls -la builds/rpi/brsaudio/tmp/deploy
+
+find builds/rpi/brsaudio/tmp/deploy 
