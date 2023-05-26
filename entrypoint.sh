0d461f013206dbe9fd7559a9e79b2ea19fe2e914
Ben Payne <ben@bluerocksoft.com>
2023-05-26 11:09:15 -0700

    Print original UID/GID

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ echo $1 $2
 orig_user=$(stat -c '%u' $GITHUB_WORKSPACE )
 orig_group=$(stat -c '%g' $GITHUB_WORKSPACE )
 
+echo "User ID " $orig_user
+echo "Group ID " $orig_group
+
 # set all files to the user yocto so we can run the build as this user.
 chown -R yocto:yocto $GITHUB_WORKSPACE
 
