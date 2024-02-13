9af6e20a0040fe944c0193b2840f5ca200ab809c
Stephen Anthony Jones <stephen@bluerocksoft.com>
2024-02-12 19:22:23 -0800

    Set exit code of docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,15 @@ chown -R yocto:yocto $GITHUB_WORKSPACE
 
 # run build script at the yocto user (created in the Dockerfile)
 sudo -i -u yocto /build.sh $1 $2 $3 $GITHUB_WORKSPACE
+exit_status=$?
 
 # set all files back to previous user
 chown -R $orig_user:$orig_group $GITHUB_WORKSPACE
+
+if [ $exit_status -eq 0 ]; then
+    echo "Yocto build completed successfully"
+    exit 0
+else
+    echo "Bitbake failed!"
+    exit 1
+fi
